5db7c9d4568d175f4d4db06e2e64cd5496c3acd6
Md Nahidul Islam <183149325+wnzid@users.noreply.github.com>
2025-05-23 13:57:32 +0300

    v1.1

--- ScannerA/Program.cs
+++ ScannerA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ScannerA
 {
@@ -12,7 +13,22 @@ namespace ScannerA
             Console.WriteLine("         ScannerA is starting up         ");
             Console.WriteLine("=========================================\n");
 
-            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("This program will scan .txt files in a folder.");
+            Console.WriteLine("Let’s start by choosing the folder to scan.\n");
+
+            Console.Write("Enter the full path to the folder: ");
+            string folderPath = Console.ReadLine();
+
+            if (Directory.Exists(folderPath))
+            {
+                Console.WriteLine("\nThe folder exists. We’re good to go!");
+            }
+            else
+            {
+                Console.WriteLine("\nThe folder doesn’t exist. Please check the path and try again.");
+            }
+
+            Console.WriteLine("\nPress any key to close...");
             Console.ReadKey();
         }
     }
