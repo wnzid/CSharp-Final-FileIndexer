8751fd0e36047d782913fe919b248f9e930c459c
Md Nahidul Islam <183149325+wnzid@users.noreply.github.com>
2025-05-24 09:57:24 +0300

    v1.2

--- ScannerA/Program.cs
+++ ScannerA/Program.cs
@@ -19,13 +19,28 @@ namespace ScannerA
             Console.Write("Enter the full path to the folder: ");
             string folderPath = Console.ReadLine();
 
-            if (Directory.Exists(folderPath))
+            if (!Directory.Exists(folderPath))
             {
-                Console.WriteLine("\nThe folder exists. We’re good to go!");
+                Console.WriteLine("\nThe folder doesn’t exist. Please check the path and try again.");
             }
             else
             {
-                Console.WriteLine("\nThe folder doesn’t exist. Please check the path and try again.");
+                Console.WriteLine("\nFolder found. Scanning for .txt files...\n");
+
+                string[] txtFiles = Directory.GetFiles(folderPath, "*.txt");
+
+                if (txtFiles.Length == 0)
+                {
+                    Console.WriteLine("No .txt files found in the folder.");
+                }
+                else
+                {
+                    Console.WriteLine("Found the following .txt files:");
+                    foreach (string file in txtFiles)
+                    {
+                        Console.WriteLine("- " + Path.GetFileName(file));
+                    }
+                }
             }
 
             Console.WriteLine("\nPress any key to close...");
