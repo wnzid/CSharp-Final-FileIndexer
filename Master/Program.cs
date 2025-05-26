19d69b98fb3c0d71185ef9a5d67014a0b0d27200
Md Nahidul Islam <183149325+wnzid@users.noreply.github.com>
2025-05-26 11:05:40 +0300

    v2.0

--- Master/Program.cs
+++ Master/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace Master
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Master - Named Pipe Server";
+
+            Console.WriteLine("=========================================");
+            Console.WriteLine("         Master is waiting...            ");
+            Console.WriteLine("=========================================\n");
+
+            string pipeName = "agent1";
+
+            using NamedPipeServerStream pipeServer = new NamedPipeServerStream(pipeName, PipeDirection.In);
+
+            Console.WriteLine($"Waiting for connection on pipe: {pipeName}");
+            pipeServer.WaitForConnection();
+            Console.WriteLine("Connected to ScannerA.\n");
+
+            using StreamReader reader = new StreamReader(pipeServer, Encoding.UTF8);
+
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine("Received: " + line);
+            }
+
+            Console.WriteLine("\nConnection closed. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
